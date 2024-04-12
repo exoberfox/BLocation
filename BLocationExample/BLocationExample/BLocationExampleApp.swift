bef3dd53c33f534f8a2dd647dde39dd2143d1b56
Mikhail Chernikov <exoberfox@gmail.com>
2024-04-12 21:15:13 +0200

    Example project init

---
+++ BLocationExample/BLocationExample/BLocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLocationExampleApp.swift
+//  BLocationExample
+//
+//  Created by Михаил Черников on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BLocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
